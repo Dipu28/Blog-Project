e355e6dcddec3757cd19ac1c90410fffb68d52b0
Dipu28 <dipanshusinghlove@gmail.com>
2020-08-25 21:42:17 +0530

    Added the CRUD

---
+++ blog/blogapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.HomeView.as_view(),name='home'),
+    path('article/<int:pk>',views.ArticleDetailView.as_view(),name='article-detail'),
+    path('add_post/',views.AddPostView.as_view(),name = 'add_post'),
+    path('article/edit/<int:pk>',views.UpdatePostView.as_view(),name = 'update_post'),
+    path('article/<int:pk>/remove',views.DeletePostView.as_view(),name = 'delete_post'),
+]
