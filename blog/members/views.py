8cc52c2acc6b0399bbb7c9a8f3da35079cdfacc3
Dipu28 <dipanshusinghlove@gmail.com>
2020-08-27 22:28:22 +0530

    User Profile

--- blog/members/views.py
+++ blog/members/views.py
@@ -1,11 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm ,EditProfileForm,PasswordChangingForm
+from .forms import SignUpForm ,EditProfileForm,PasswordChangingForm, ProfilePageForm
+from blogapp.models import Profile
 
 # Create your views here.
+
+class CreateProfilePageView(generic.CreateView):
+    model = Profile
+    form_class = ProfilePageForm
+    template_name = 'registration/create_user_profile_page.html'
+    # fields = '__all__'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = [ 'bio','profile_pic', 'facebook_url', 'twitter_url', 'instagram_url','pinterest_url']
+    success_url = reverse_lazy('home')
+
+
+class ShowProfileView(generic.DetailView):
+    model = Profile 
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        # We don't want all the profile /// We want the specific id 
+        # users = Profile.objects.all()
+        context = super(ShowProfileView, self).get_context_data(*args, **kwargs)
+
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context["page_user"] = page_user
+        return context
+
+
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
     # form_class = PasswordChangeForm
@@ -15,6 +49,7 @@ class PasswordsChangeView(PasswordChangeView):
 
 def password_success(request):
     return render(request, 'registration/password_success.html',{})
+
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
