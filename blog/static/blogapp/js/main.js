f1db326a06beb569fa549b782c8cda43ef357b21
Dipu28 <dipanshusinghlove@gmail.com>
2020-08-30 18:48:10 +0530

    Responsive

--- blog/static/blogapp/js/main.js
+++ blog/static/blogapp/js/main.js
@@ -1,12 +1,35 @@
+const responsive={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+
 $(document).ready(function(){
     // Owl-crousel for blog
     $('.owl-carousel').owlCarousel({
         loop:true,
         autoplay:true,
         autoplayTimeout:4000,
-        dots:false,
-        nav:true,
-        navText:[$('.owl-navigation.owl-nav-prev'),$('.owl-navigation.owl-nav-next')]
+        dots:true,
+        // nav:true,
+        // navText:[$('.owl-navigation.owl-nav-prev'),$('.owl-navigation.owl-nav-next')]
+        responsive:responsive
     });
-
+    
+    // Click to scroll top
+    $('.move-up.my').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },1);
+    });
 });
+
