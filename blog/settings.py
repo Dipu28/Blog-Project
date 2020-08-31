24fdaa7ba3b10364ef88dcc8c9d94d4976a3d7de
Dipu28 <dipanshusinghlove@gmail.com>
2020-08-31 18:43:46 +0530

    wsgi

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8wypudsc$cxr%y4f3_u8juxgi1g!z&cc5diki2o!%jll!nb)xl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['app-blog-trouble.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['app-blog-trouble.herokuapp.com','127.0.0.1']
 
 
 # Application definition
