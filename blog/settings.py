898f077860b304a656e368f86763889c12c0da17
Dipu28 <dipanshusinghlove@gmail.com>
2020-08-31 18:50:22 +0530

    Deploying

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8wypudsc$cxr%y4f3_u8juxgi1g!z&cc5diki2o!%jll!nb)xl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['app-blog-trouble.herokuapp.com','127.0.0.1']
 
